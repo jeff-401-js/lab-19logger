87cbc29acd7846bff721ccf6c3bc79286f866004
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-20 18:35:54 -0700

    logger db work

--- logger.js
+++ logger.js
@@ -7,3 +7,14 @@ const Q = require('@nmq/q/client');
 const file = new Q('files');
 const db = new Q('database');
 
+const dbEvents = ['create', 'read', 'update', 'delete', 'error'];
+
+dbEvents.forEach(event => {
+  db.subscribe(event, payload => {
+    if(event === 'error'){
+      console.error(`${event} happened`, payload);
+    }else{
+      console.log(`${event} happened`, payload);
+    }
+  });
+});
